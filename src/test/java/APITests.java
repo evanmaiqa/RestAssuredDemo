499e467155f47cbb34710a8bd5ed72d1aa1014bc
Evan Mai <mai.evan@yahoo.com>
2023-02-19 15:05:46 -0800

    upd

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -7,15 +7,33 @@ import static io.restassured.RestAssured.given;
 public class APITests {
     // https://www.youtube.com/watch?v=Orn8cP1yRJc
     @Test
-    public void statusTest2() {
-        given().
-                get("http://demo.guru99.com").
-                then().
-                statusCode(200);
+    public void statusTest() {
+        given()
+                .when()
+                .get("http://demo.guru99.com")
+                .then()
+                .statusCode(200);
     }
 
     @Test
     public void statusLineTest() {
         Assert.assertEquals(RestAssured.get("http://demo.guru99.com").getStatusLine(), "HTTP/1.1 200 OK");
+    }
+    @Test
+    public void getStatusTextTest() {
+        given()
+                .given()
+                .get("http://demo.guru99.com")
+                .then()
+                .log()
+                .status();
+    }
+    @Test
+    public void statusTextTest() {
+        given()
+                .given()
+                .get("http://demo.guru99.com")
+                .then()
+                .statusLine("HTTP/1.1 200 OK");
     }
 }
